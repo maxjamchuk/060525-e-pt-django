90b8b46b6dc0273b6cf1cfad51ffa0bc79af9115
Maks Iamchuk <maks.iamchuk@outlook.com>
2025-09-11 22:49:50 +0500

    Урок 2: собственный urls.py в news и функция include

--- itg/urls.py
+++ itg/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from news import views
 
@@ -7,5 +7,15 @@ from news import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main),
-    path('info/', views.info)
+    path('info/', views.info),
+    path('news/', include('news.urls'))
 ]
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.main),
+#     path('info/', views.info),
+#     path('news/', views.get_all_news),
+#     path('news/faq/', views.faq),
+#     path('news/<int:news_id>/, views.get_news_by_id)
+# ]
--- news/views.py
+++ news/views.py
@@ -7,3 +7,17 @@ def main(request):
 
 def info(request):
     return HttpResponse('information page')
+
+
+def get_all_news(request):
+    return HttpResponse('<h1>Все новости</h1>')
+
+
+def faq(request):
+    return HttpResponse('Список вопросов и ответов')
+
+
+def get_news_by_id(request, news_id):
+    if news_id > 10:
+        return HttpResponse('Такой новости нет', status=404)
+    return HttpResponse(f'Вы открыли новость #{news_id}')  # Вернёт страницу с надписью Вы открыли новость
